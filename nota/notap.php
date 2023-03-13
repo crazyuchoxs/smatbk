60f5deda7f6a65f13a94c8cb3684cb4e955e62a2
crazyuchoxs <36880442+crazyuchoxs@users.noreply.github.com>
2023-03-13 09:45:58 +0700

    nota-lagi

--- nota/notap.php
+++ nota/notap.php
@@ -3,27 +3,44 @@
     <?php include '../cetak/header.php' ?>
     <div class="container P-2">
         <span>Tanggal : 11 Maret 2023</span>
-        <table class="table table-bordered p-2">
+        <table class="table table-borderless p-2">
             <thead>
                 <tr>
-                    <th class='p-1'>No</th>
-                    <th class='p-1'>Nama Barang</th>
-                    <th class='p-1'>Qty</th>
-                    <th class='p-1'>Satuan</th>
-                    <th class='p-1'>Harga Satuan</th>
-                    <th class='p-1'>Total Harga</th>
+                    <th class='p-1 border border-2 border-dark'>No</th>
+                    <th class='p-1 border border-2 border-dark'>Nama Barang</th>
+                    <th class='p-1 border border-2 border-dark'>Qty</th>
+                    <th class='p-1 border border-2 border-dark'>Satuan</th>
+                    <th class='p-1 border border-2 border-dark'>Harga Satuan</th>
+                    <th class='p-1 border border-2 border-dark'>Total Harga</th>
                 </tr>
             </thead>
-            <tbody>
-                <tr>
-                    <th class='ps-1'>1</th>
-                    <td class='ps-1'>Beras</th>
-                    <td class='ps-1'>4</th>
-                    <td class='ps-1'>Kg</th>
-                    <td class='ps-1'>250000</th>
-                    <td class='ps-1'>100000</th>
+            <tbody class="border-bottom border-1 border-dark">
+                <?php include('../conn.php');
+                $totalAkhir = 0;
+                $rslt = mysqli_query($conn, $sqlNota);
+                if(mysqli_num_rows($rslt)>0):
+                while($rws = mysqli_fetch_assoc($rslt)) : 
+                $total = $rws['Qty'] * $rws['Harga'] ?>
+                <tr >                 
+                    <th class='ps-1 border-start border-end border-dark'><?= $rws['No'] ?></th>
+                    <td class='ps-1 border-start border-end border-dark'><?= $rws['Nama Barang'] ?></th>
+                    <td class='ps-1 border-start border-end border-dark'><?= $rws['Qty'] ?></th>
+                    <td class='ps-1 border-start border-end border-dark'><?= $rws['Satuan'] ?></th>
+                    <td class='ps-1 border-start border-end border-dark'><?= "Rp. " . number_format($rws['Harga'], 0, ',', '.') . ",00";?></th>
+                    <td class='ps-1 border-start border-end border-dark'><?= "Rp. " . number_format($total, 0, ',', '.') . ",00"; ?></th>                  
                 </tr>
+                <?php 
+                $totalAkhir = $totalAkhir + $total;
+                endwhile; 
+                endif;
+                ?>
             </tbody>
+            <tfoot>
+                <tr>
+                    <th colspan="5" class="border border-1 border-dark">Total</th>
+                    <td class="border border-1 border-dark"><?= "Rp. " . number_format($totalAkhir, 0, ',', '.'); ?></td>
+                </tr>
+            </tfoot>
 
         </table>
     </div>
