d66c6a1009abed98ad9d416f74db53f2ae9a016c
crazyuchoxs <36880442+crazyuchoxs@users.noreply.github.com>
2023-03-13 07:37:13 +0700

    nota

---
+++ nota/notap.php
@@ -0,0 +1,34 @@
+<?php include '../headutama.php'; ?>
+<div class="a4 bg-white justify-content-center" style="margin: auto;">
+    <?php include '../cetak/header.php' ?>
+    <div class="container P-2">
+        <span>Tanggal : 11 Maret 2023</span>
+        <table class="table table-bordered p-2">
+            <thead>
+                <tr>
+                    <th class='p-1'>No</th>
+                    <th class='p-1'>Nama Barang</th>
+                    <th class='p-1'>Qty</th>
+                    <th class='p-1'>Satuan</th>
+                    <th class='p-1'>Harga Satuan</th>
+                    <th class='p-1'>Total Harga</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th class='ps-1'>1</th>
+                    <td class='ps-1'>Beras</th>
+                    <td class='ps-1'>4</th>
+                    <td class='ps-1'>Kg</th>
+                    <td class='ps-1'>250000</th>
+                    <td class='ps-1'>100000</th>
+                </tr>
+            </tbody>
+
+        </table>
+    </div>
+    <div class="kaki">
+        <?php include '../cetak/footer.php' ?>
+    </div>
+</div>
+<?php include '../footutama.php'; ?>
